e95a8316301b391ca9b273d72246918d289c6e25
Wallace H Wu <wallace_h_wu@wistron.com>
2020-06-01 10:56:47 +0800

    update use ref case

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import ReactDOM from 'react-dom';
 // import {UseState} from './case/use-state/use-state';
 // import {UseEffect} from './case/use-effect/use-effect';
 // import {UseReducer} from './case/use-reducer/use-reducer';
-import {UseRefInput} from './case/use-ref/use-ref';
+// import {UseRefInput} from './case/use-ref/use-ref';
+import {UseRefForm} from './case/use-ref/use-ref';
 import * as serviceWorker from './common/serviceWorker';
 import './style/index.css';
 
 ReactDOM.render(
     <div>
-        <UseRefInput/>
+        <UseRefForm/>
     </div>
     , document.getElementById('root'));
 serviceWorker.unregister();
